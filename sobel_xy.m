bf1c872dbe0f56b8bc50de141c87875c6097c795
ShangyinGao <shangyingao@gmail.com>
2017-05-09 16:29:12 +0200

    Update sobel_xy.m

--- sobel_xy.m
+++ sobel_xy.m
@@ -11,8 +11,8 @@ function [Fx,Fy] = sobel_xy(Image)
 %     end
 %  end
 sobel_y=[1,2,1;0,0,0;-1,-2,-1];
- sobel_x=sobel_y';
- Fx=conv2(Image,sobel_x,'same');
+sobel_x=sobel_y';
+Fx=conv2(Image,sobel_x,'same');
 Fy=conv2(Image,sobel_y,'same');
 
 
